d786c8243c80f3f007fc74f1075925436cfd343b
georgeSEC1 <111563036+georgeSEC1@users.noreply.github.com>
2023-02-14 21:45:42 +1100

    Add files via upload

---
+++ ritemath.py
@@ -0,0 +1,4 @@
+while(True):
+      income = input("total currency per time period: ")
+      remaining = input("total remaining at the end of the time period: ")
+      print("Spent: $",int(income)-int(remaining))
